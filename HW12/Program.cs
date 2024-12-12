a9bd7170f924e72a2ec1d302253da24df0dcd7d6
leonychka <leonid.shkop@gmail.com>
2024-12-12 17:14:43 +0300

    kurt cobain

---
+++ HW12/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 6;
+int b = 2;
+int sum3 = a * b;
+Console.WriteLine(sum3);
+
+int c = 3;
+int d = 1;
+int sum4 = c + d;
+Console.WriteLine(sum4);
+
+long sum5 = sum3 / sum4;
+Console.WriteLine(sum5);
