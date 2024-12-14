429e4dc09d6343d85ea8d5b2bd2e73b1fccca5bd
leonychka <leonid.shkop@gmail.com>
2024-12-14 14:29:15 +0300

    5 kettlebells

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 8;
+int b = 11;
+int c = 77;
+int d = 5;
+int e = 43;
+
+int max = a;
+
+if (b > max)
+{
+    max=b;
+}
+if (c > max)
+{
+    max=c;
+}
+if (d > max)
+{
+    max=d;
+}
+if (e > max)
+{
+    max=e;
+}
+Console.WriteLine(max);
